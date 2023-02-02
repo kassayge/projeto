3deabeb5400957312fc01d5be91ec76e9a9f74ce
Kassayge <116615755+kassayge@users.noreply.github.com>
2023-02-02 12:08:50 -0300

    tivemos problemas como envio de imagens (#7)

--- aaaaaaaaaaaa/gustavo/perfilDO.php
+++ aaaaaaaaaaaa/gustavo/perfilDO.php
@@ -3,15 +3,16 @@ include "conexao.php";
 session_start();
 
 $uploaddir = './static/perfil_img/'; 
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+$uploadfile = $uploaddir. basename($_FILES['userfile']['name']);
 
 $img = $_FILES['userfile']['name'];
-rename($img, 'avatar-'. $_SESSION['usuariologin']);
+//rename($img, 'avatar-'. $_SESSION['usuariologin']);
 $usu = $_SESSION['usuariologin'];
-$sql = "UPDATE INTO `imagens`(`id`, `login`, `img`) VALUES (null, '$usu', '$img')";
+$sql = "UPDATE `imagens` SET `login` = '$usu', `img` = '$img')";
 $envio = mysqli_query($link, $sql);
 
-#echo $_files['userfile']['tmp_name'];
+echo $_FILES['userfile']['name'];
+echo $_FILES['userfile']['tmp_name'];
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "deu certo";
@@ -21,10 +22,10 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 
 
 if($envio){
-    echo "<script>window.location='index.php';alert('imagem de perfil enviada com sucesso');</script>";
+    //echo "<script>window.location='index.php';alert('imagem de perfil enviada com sucesso');</script>";
     
    }else {
-    echo "<script language='javascript' type='text/javascript'>alert('falhou:c');window.location.href='perfil.php'</script>";
+    //echo "<script language='javascript' type='text/javascript'>alert('falhou:c');window.location.href='perfil.php'</script>";
 }
 ?>
 
