288b7e3554f76b0c402d4c4cd66937080f462773
kassayge <gugafiebes@gmail.com>
2023-02-02 12:08:26 -0300

    tivemos problemas como envio de imagens

--- aaaaaaaaaaaa/gustavo/perfil.php
+++ aaaaaaaaaaaa/gustavo/perfil.php
@@ -3,12 +3,12 @@ include "base.php";
 ?>
 
 <body class="sobre"> 
-        <form enctype="multipart/form-data" action="perfilDO.php" method="post">
-            <div class='img'>
-                <label for='file'>adicione sua foto de perfil</label>
-                <input name="userfile" type="file" id='file' class='perfilA' accept=".jpg, .png" /> 
-                <input type="submit" value="Enviar" accept=".jpg .png" class='inputB'/>
-            </div>
-        </form>  
+    <form action="perfilDO.php" method="post" enctype="multipart/form-data">
+        <div class='img'>
+            <label for='file'>adicione sua foto de perfil</label>
+            <input name="userfile" type="file" id='file' class='perfilA' accept=".jpg, .png" /> 
+            <input type="submit" value="Enviar" accept=".jpg .png" class='inputB'/>
+        </div>
+    </form>  
 </body>
 </html>
