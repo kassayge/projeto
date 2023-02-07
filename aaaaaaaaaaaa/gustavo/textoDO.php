50ff67ac98b3ffaa0189c80390de0b26be37f21e
Kassayge <116615755+kassayge@users.noreply.github.com>
2023-02-07 14:10:35 -0300

    Update textoDO.php

--- aaaaaaaaaaaa/gustavo/textoDO.php
+++ aaaaaaaaaaaa/gustavo/textoDO.php
@@ -4,8 +4,4 @@ require_once ("conexao.php");
 $sql = "SELECT * FROM `comentarios` ORDER BY id desc"; //WHERE login = '$usuario' modificação so pro usuario logado
 $texto = mysqli_query($link, $sql);
 
-?>
-
-    
-        
-    
+?>   
